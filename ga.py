bc7f85e2b395183ec7e8229d05769bd0e05df37f
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-08-20 18:00:12 +0600

    Add files via upload
    Only Good Article Management

--- ga.py
+++ ga.py
@@ -1,28 +1,4 @@
-import re
-import json
-import datetime as dt
-import time
-import pywikibot as pb
-import os
-from PIL import Image as im
-bn = pb.Site("bn","wikipedia")
-def en2bn(txt):
-    refs = ["০", "১","২","৩","৪","৫","৬","৭","৮","৯"]
-    txt = str(txt)
-    st=""
-    for i in txt:
-        if(i.isdigit()):
-            st+=refs[int(i)]
-        else:
-            st+=i
-    return st
-now = dt.datetime.now()
-yesterday = now - dt.timedelta(days=1)
-config = json.loads(pb.Page(bn,"user:নকীব বট/config.json").text)
-r = pb.data.api.Request
-ISO = "%Y-%m-%dT%H:%M:%SZ"
-months = "((জান|ফেব্র)ুয়ার[িী]|ম(ে|ার্চ)|এপ্রিল|জু(ন|লা[ইঈ])|[অআ]গা?[সষ]্ট|((সেপ্ট|ন[বভ]|ডিস)েম্|অক্টো)[ভব]র)"
-no ="০১২৩৪৫৬৭৮৯"
+
 classes = re.compile("\|\s*(?P<p>(মান|শ্রে[ণন]ী|[cC]lass|[qQ]uality))\s*=[^\|\}]*")
 rea_template = re.compile("\{\{\s*ভালো নিবন্ধ পুনর্মূল্যায়ন\s*\|\s*(?:1=\s*)?(?P<topic>[^\|]+)\|\s*(?:2=\s*)?(?P<page>\S{1,2})\s*\|\s*(?:3=)?(?P<user>[^\|\}]+)[^}]*\}\}")
 entry_pref = "#\s*\{\{\s*([Gg]ANentry|প্রভানিভুক্তি)\s*\|\s*(1\s*=\s*)"
