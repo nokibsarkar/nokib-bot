d611f4f81859a66978f96e040720fab364a27c14
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-08 16:44:05 +0600

    Add files via upload

--- mass_move.py
+++ mass_move.py
@@ -1,7 +1,7 @@
 from environment import *
 # previously from setup import *
 summary = u'[[উইকিপিডিয়া:বট/অনুমোদনের অনুরোধ/নকীব বট/কাজ ৩|আলোচনাসাপেক্ষে]] বট কর্তৃক [[%s]] হতে {{মুক্ত নয় হ্রাসকৃত}} অপসারণ'
-cats = pb.Category('বিষয়শ্রেণী:৭ দিন পূর্বের হ্রাসকৃত অ-মুক্ত ফাইল').members()
+cats = pb.Category(bn,'বিষয়শ্রেণী:৭ দিন পূর্বের হ্রাসকৃত অ-মুক্ত ফাইল').members()
 subst = re.compile('\s*\{\{ *মুক্ত নয় হ্রাসকৃত[^\}]+\}\}\s*')
 def main():
     for i in cats:
