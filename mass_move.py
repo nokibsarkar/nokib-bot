cb6e43cfd711fad7845cb4a1d9f2355eea88c3ad
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-08 16:41:08 +0600

    Add files via upload

--- mass_move.py
+++ mass_move.py
@@ -1,18 +1,9 @@
 from environment import *
 # previously from setup import *
-summary = u'[[উইকিপিডিয়া:বট/অনুমোদনের অনুরোধ/নকীব বট/কাজ ৩|আলোচনাসাপেক্ষে]] বট কর্তৃক [[%s]] হতে [[%s]] - নামে স্থানান্তর'
-to_move = re.compile(
-	'# *\[\[ *([^\]]+)\]\] *- *\[\[([^\]]+)\]\]'
-	)
-def move(m):
-    source = pb.Page(bn,m.group(1).strip())
-    target = m.group(2).strip()
-    source.move(
-    	target,
-    	reason = summary % (m.group(1), target),
-    	movetalk = True
-    	)
+summary = u'[[উইকিপিডিয়া:বট/অনুমোদনের অনুরোধ/নকীব বট/কাজ ৩|আলোচনাসাপেক্ষে]] বট কর্তৃক [[%s]] হতে {{মুক্ত নয় হ্রাসকৃত}} অপসারণ'
+cats = pb.Category('বিষয়শ্রেণী:৭ দিন পূর্বের হ্রাসকৃত অ-মুক্ত ফাইল').members()
+subst = re.compile('\s*\{\{ *মুক্ত নয় হ্রাসকৃত[^\}]+\}\}\s*')
 def main():
-    page = pb.Page(bn,'ব্যবহারকারী:Nokib Sarkar/খেলাঘর২')
-    page.text = to_move.sub('', move)
-    page.save('সফল!!!!!')
+    for i in cats:
+        i.text = subst.sub('',i.text)
+        i.save(summary)
